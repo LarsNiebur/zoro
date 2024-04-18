a9f372dec09c7448aa947f9b3e39284df4f8aa4b
MrPink <larsniebur78@gmail.com>
2024-04-18 12:24:54 +0200

    renewToken in Token Class implementiert und aufgerufen

--- app/core/Token.php
+++ app/core/Token.php
@@ -1,13 +1,19 @@
 <?php
 namespace App\Core;
-
+ 
 use GemLibrary\Helper\WebHelper;
 use GemLibrary\Http\GemRequest;
 use GemLibrary\Http\GemToken;
 use GemLibrary\Http\JsonResponse;
 
+/**
+ * @function createToken(int $user_id):JsonResponse
+ * @function validate():bool
+ * @function renewToken():JsonResponse
+ */
+ 
 class Token extends GemToken {
-
+ 
     private GemRequest $request;
     private JsonResponse $response;
     public function __construct(GemRequest $request)
@@ -18,27 +24,61 @@ class Token extends GemToken {
         $this->request = $request;
         $this->iss = "";
         //TODO:
-        //In Production Remove following line of code 
+        //In Production Remove following line of code
         $this->ip = "213.213.213.213";
          //TODO:
-        //In Production Remove following line of code 
+        //In Production Remove following line of code
         $this->userAgent = "Some_User_Agent";
-
+ 
         $this->type = "";
         //TODO:
-        //In Production Uncomment following codes 
+        //In Production Uncomment following codes
         //$this->userAgent = $this->request->userMachine;
         //$this->ip = $this->request->remoteAddress;
-
+ 
     }
-
+ 
     public function createToken(int $user_id):JsonResponse
     {
         $token = $this->create($user_id,$_ENV['TOKEN_VALIDATION_IN_SECONDS']);
         return $this->response->success($token,1,'token generated successfully');
     }
-
+ 
     public function validate():bool
+    {
+        $tokenString = $this->getPureTokenFromHeader();
+        if(!$tokenString)
+        {
+            return false;
+        }
+        return $this->verify($tokenString);
+    }
+ 
+    public function renewToken(): JsonResponse
+    {
+        //TODO::
+        //1: check if token ist kurz to ablauf
+        //2: if token hat genug zeit ; answer => token hat noch 5 minuten zeit ,
+        //3: run following code
+        $tokenString = $this->getPureTokenFromHeader();
+        if(!$tokenString)
+        {
+            return $this->response->forbidden('not token found in header');
+        }
+        if(!$this->verify($tokenString))
+        {
+            return $this->response->unauthorized($this->error);
+        }
+       
+        $tokenString = $this->renew($tokenString,$_ENV['TOKEN_VALIDATION_IN_SECONDS']);
+        if(!$tokenString)
+        {
+            return $this->response->internalError('please try again');
+        }
+        return $this->response->success($tokenString,1,'successfully generated new token');
+    } 
+ 
+    private function getPureTokenFromHeader():false|string
     {
         $token_in_header = $this->request->authorizationHeader;
         if(!is_string($token_in_header)   || strlen($token_in_header) < 30)
@@ -50,6 +90,6 @@ class Token extends GemToken {
         {
             return false;
         }
-        return $this->verify($token_in_header);
-    }
+        return $token_in_header;
+    }   
 }
--- app/service/Authenticate.php
+++ app/service/Authenticate.php
@@ -1,40 +1,40 @@
 <?php
 namespace App\Service;
 use App\Controller\UserController;
+use App\Core\Token;
 use GemLibrary\Http\GemRequest;
 use GemLibrary\Http\JsonResponse;
-
+ 
 class Authenticate {
-
+ 
     private GemRequest $request;
-
+ 
     public function __construct(GemRequest $request)
     {
         $this->request = $request;
     }
-
+ 
     public function loginByEmail():JsonResponse
-    { 
+    {
         $userController = new UserController($this->request);
         return $userController->loginByEmail();
     }
-
+ 
     public function register():JsonResponse
     {
        $userController = new UserController($this->request);
-       return $userController->register(); 
+       return $userController->register();
     }
-
+ 
     public function loginByToken():JsonResponse
     {
         $userController = new UserController($this->request);
         return $userController->loginByToken();
     }
-
+ 
     public function renewToken():JsonResponse
     {
-        //1: Token existiert
-        //2: Token ist validiert
-        //3: wenn vadidiert generiere neues Token mit extra Zeit
+        $tokenClass = new Token($this->request);
+        return $tokenClass->renewToken();
     }
 }
